59fb233b2f2667dc0a56178922ac432c1013b33d
pillionpollarprize <creeftus@gmail.com>
2025-01-07 20:21:00 +0200

    optimized sprite projection
    still working on it, my brain is melting every second

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -14,7 +14,12 @@ public class Enemy : MonoBehaviour
     }
     void FixedUpdate()
     {
+        var targetRotation = target.position - transform.position;
+        targetRotation.y = 0;
+        Quaternion rotation = Quaternion.LookRotation(targetRotation);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 10 * Time.deltaTime);
         var targetPosition = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
         rb.MovePosition(targetPosition);
+        
     }
 }
--- Assets/Scripts/SpriteProjector.cs
+++ Assets/Scripts/SpriteProjector.cs
@@ -1,46 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.TextCore.Text;
 
 public class SpriteProjector : MonoBehaviour
 {
     private SpriteRenderer spriteRenderer;
-    public Transform plane;
     public GameObject look;
 
-    private float step = 45f;
-
     public Sprite N, W, S, E;
+
     public void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
-        
     }
+
     public void Update()
     {
-        // this uses a plane that points out a vector outwards from a point as if looking from the bottom to the top 
-        // explaination: https://dustinanglin.medium.com/now-youre-just-projecting-7f93dbdc9378
-        var projected = Vector3.ProjectOnPlane(look.transform.forward, plane.up);
+        var directionToPlayer = (look.transform.position - transform.position).normalized;
+        float angle = Mathf.Atan2(directionToPlayer.x, directionToPlayer.z) * Mathf.Rad2Deg;
 
-        // this finds an angle between two vectors
-        var angle = Vector3.SignedAngle(projected, plane.forward, plane.up);
+        // normalize the angle to a range of [0, 360)
+        if (angle < 0) angle += 360;
 
-        // absolute value so no minuses
-        var AbsAngle = Mathf.Abs(angle);
-
-
-        if (AbsAngle < step) spriteRenderer.sprite = N;
-        else if (AbsAngle < step * 3) spriteRenderer.sprite = Mathf.Sign(angle) < 0 ? W : E;
-        else spriteRenderer.sprite = S;
+        if (angle >= 315 || angle < 45) // north
+        {
+            spriteRenderer.sprite = N;
+        }
+        else if (angle >= 45 && angle < 135) // east
+        {
+            spriteRenderer.sprite = E;
+        }
+        else if (angle >= 135 && angle < 225) // south
+        {
+            spriteRenderer.sprite = S;
+        }
+        else if (angle >= 225 && angle < 315) // west
+        {
+            spriteRenderer.sprite = W;
+        }
         Billboard(spriteRenderer.transform, look);
     }
-    public void Billboard(Transform thing, GameObject mainCamera)
-    {
-        var dir = plane.position - mainCamera.transform.position;
-        var LookAtRotation = Quaternion.LookRotation(dir);
 
-        var LookAtRotationOnly_Y = Quaternion.Euler(thing.rotation.eulerAngles.x, LookAtRotation.eulerAngles.y, thing.eulerAngles.z);
-        thing.rotation = LookAtRotationOnly_Y;
+    private void Billboard(Transform thing, GameObject mainCamera)
+    {
+        var dir = mainCamera.transform.position - thing.position;
+        var lookAtRotation = Quaternion.LookRotation(-dir, Vector3.up);
+        thing.rotation = Quaternion.Euler(0, lookAtRotation.eulerAngles.y, 0);
     }
 }
