107921a59b49f33e0ec3cb829be901e15c941b5f
pillionpollarprize <creeftus@gmail.com>
2025-01-14 20:59:37 +0200

    basic ammo system

--- Assets/Scripts/Arsenal.cs
+++ Assets/Scripts/Arsenal.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Arsenal : MonoBehaviour
@@ -7,11 +8,11 @@ public class Arsenal : MonoBehaviour
     [Header("General")]
     public Transform bulletSpawn;
     public bool isReadyToShoot;
+    public TextMeshProUGUI ammoText;
     public int[] ammunition = new int[6];
     public bool hasRKeycard;
     public bool hasGKeycard;
     public bool hasBKeycard;
-    [HideInInspector] public int currentWeapon;
     [Header("Pistol")]
     public GameObject pistolBulletPrefab;
     public float pistolCooldown;
@@ -23,16 +24,20 @@ public class Arsenal : MonoBehaviour
     public float fistCooldown;
     [HideInInspector]public int fistID = 1;
 
-
+    int currentWeapon;
     float currentCooldown;
     int gunAmmo;
 
     //InvokeRepeating("Shoot", 0, fireRate);
     private void Start()
     {
+        ammunition[1] = 1;
+        ammunition[2] = 50;
+        gunAmmo = ammunition[2];
         isReadyToShoot = true; 
         currentWeapon = 2;
         currentCooldown = pistolCooldown;
+        ammoText.text = gunAmmo.ToString();
     }
     private void Update()
     {
@@ -41,15 +46,20 @@ public class Arsenal : MonoBehaviour
             currentWeapon = fistID;
             print("current weapon: 1");
             currentCooldown = fistCooldown;
+            gunAmmo = ammunition[fistID];
+            ammoText.text = gunAmmo.ToString();
         }
         if (Input.GetKeyDown(KeyCode.Alpha2)) 
         {
             currentWeapon = pistolID;
             print("current weapon: 2");
             currentCooldown = pistolCooldown;
+            gunAmmo = ammunition[pistolID];
+            ammoText.text = gunAmmo.ToString();
         }
-        if (Input.GetMouseButtonDown(0) && isReadyToShoot)
+        if (Input.GetMouseButton(0) && isReadyToShoot && gunAmmo > 0)
         {
+            isReadyToShoot = false;
             switch (currentWeapon){
                 case 1:
                     ShootFist();
@@ -60,7 +70,8 @@ public class Arsenal : MonoBehaviour
                     print("pistol");
                     break;
             }
-            isReadyToShoot = false;
+            gunAmmo--;
+            ammoText.text = gunAmmo.ToString();
             Invoke("ReadyToShoot", currentCooldown);
         }
     }
@@ -83,7 +94,7 @@ public class Arsenal : MonoBehaviour
         {
             print(player.rb.velocity.magnitude + "|" + player.rb.angularVelocity.magnitude);
         }
-      
+        gunAmmo++;
         //audsrc.pitch = Random.Range(0.8f, 1.1f);
         //audsrc.PlayOneShot(bulletSound);
     }
